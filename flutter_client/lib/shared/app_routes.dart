fe142b74b4e4c8d08c9e1313360983e4c63269c2
Marcelo Zarpelon <marcelinhozarpelon@gmail.com>
2026-03-26 12:16:49 -0300

    altera rota base e remove comentarios

--- flutter_client/lib/shared/app_routes.dart
+++ flutter_client/lib/shared/app_routes.dart
@@ -3,6 +3,6 @@
 class AppRoutes {
   AppRoutes._();
 
-  static const String home  = '/';
-  static const String login = '/login';
+  static const String home  = '/home';
+  static const String login = '/';
 }
