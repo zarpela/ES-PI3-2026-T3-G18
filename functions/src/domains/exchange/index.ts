5d5bd6f91eac16ba1761bd9709e09667137b0680
Miguel-Castro07 <miguelafonsocastro100@gmail.com>
2026-05-18 10:00:41 -0300

    exportacoes

--- functions/src/domains/exchange/index.ts
+++ functions/src/domains/exchange/index.ts
@@ -4,3 +4,5 @@ export * from "./handlers/buyToken";
 export * from "./handlers/buySellOrder";
 export * from "./handlers/sellToken";
 export * from "./handlers/getSellOrders";
+export * from "./handlers/getInvestments";
+export * from "./handlers/TokenValuation";
