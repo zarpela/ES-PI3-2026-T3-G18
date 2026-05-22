4a7a8f7e4a71974dc4f11ca9c5d297ce47ba7774
Miguel-Castro07 <miguelafonsocastro100@gmail.com>
2026-05-22 18:49:59 -0300

    gravação de histórico

--- functions/src/domains/startups/index.ts
+++ functions/src/domains/startups/index.ts
@@ -5,3 +5,4 @@ export { getStartupById } from "./handlers/getStartupById";
 export { createStartupQuestion } from "./handlers/createStartupQuestion";
 export { getStartupQuestions } from "./handlers/getStartupQuestions";
 export { getStartupPrivateQuestions } from "./handlers/getStartupPrivateQuestions";
+export { saveHistory} from "./schedule/saveTokenHistory";
