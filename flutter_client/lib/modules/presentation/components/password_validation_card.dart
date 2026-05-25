98837e5485a786f1452698765617efd473036068
Marcelo Zarpelon <marcelinhozarpelon@gmail.com>
2026-05-24 21:37:43 -0300

    uniformiza cores

--- flutter_client/lib/modules/presentation/components/password_validation_card.dart
+++ flutter_client/lib/modules/presentation/components/password_validation_card.dart
@@ -19,7 +19,7 @@ class PasswordValidationCard extends StatelessWidget {
       width: double.infinity,
       padding: const EdgeInsets.all(24.0),
       decoration: BoxDecoration(
-        color: const Color(0xFFF6F1FF),
+        color:  const Color(0xFFFBF1F2),
         borderRadius: BorderRadius.circular(24.0),
       ),
       child: Column(
